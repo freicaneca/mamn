f3843105f0f09db34458c94372e678552e7d2856
Thiago Moura <tmoura@gmail.com>
2015-12-04 15:50:35 -0300

    novos

---
+++ validityFunctions/i_index.m
@@ -0,0 +1,18 @@
+%%%%%%%%%%
+%
+% FUNÇÃO: i_index - I-index
+%
+% ENTRADA:
+%
+% - ind: Cromossomo com os clusters
+% - dados: Base de dados sendo utilizada
+% - e1: Constante a ser passada para a função
+%
+% SAÍDA:
+%
+% - result: o valor da função calculada
+%
+%%%%%%%%%%
+function [result] = i_index(ind, dados, e1)
+
+end
