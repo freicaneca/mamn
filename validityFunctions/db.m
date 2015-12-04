e5567a2e81d519d1fc5fac306f7c7d7ab742ff3f
Thiago Moura <tmoura@gmail.com>
2015-12-04 10:36:29 -0300

    novos

---
+++ validityFunctions/db.m
@@ -0,0 +1,20 @@
+%%%%%%%%%%
+%
+% FUNÇÃO: db - Davies-Bouldin
+%
+% ENTRADA:
+%
+% - testSet: Número de classificadores
+% - baseLearn: Tipo do classificador
+% - data: Dados de treinamento
+% - columnClass: Coluna do arquivo de dados que determina a classe das
+% observações
+%
+% SAÍDA:
+%
+% - result - o valor da função calculada
+%
+%%%%%%%%%%
+function [result] = db(dataPoints, ind)
+
+end
