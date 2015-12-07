59ef59c1ecb1c47ef2d74cf2954e1e0264e3619e
Thiago Moura <tmoura@gmail.com>
2015-12-07 10:16:17 -0300

    novos

--- core/crossover.m
+++ core/crossover.m
@@ -15,4 +15,24 @@
 %%%%%%%%%%
 function [filho1, filho2] = crossover(pai1, pai2, nPoints)
 
+    [k1,~] = size(pai1);
+    [k2,~] = size(pai2);
+    
+    if nPoints >= k1
+        error('ERROR CROSSOVER: Maior número de cortes do que a quantidade de clusters no pai');
+    end
+    
+    if (k1 > k2)  % Troca os pais entre si
+        temp = k2;
+        tempPai = pai2;
+        
+        k2 = k1;
+        k1 = temp;
+        
+        pai2 = pai1;
+        pai1 = tempPai;
+    end
+    
+    
+
 end
