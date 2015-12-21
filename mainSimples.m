4d630ca5b621b033f82b00d2a90e78a7ddd4f1db
Thiago Moura <tmoura@gmail.com>
2015-12-21 11:56:05 -0300

    x

--- mainSimples.m
+++ mainSimples.m
@@ -32,6 +32,9 @@ function [] = mainSimples(dataset, pop_size, max_gen, K)
     
     aveRIOrig = 0;
     aveRICont = 0;
+    
+    clustersOrig = zeros(K,1);
+    clustersCont = zeros(K,1);
 
     %%%%%%%%%
     % Carrega dados da base de dados passada
@@ -42,12 +45,14 @@ function [] = mainSimples(dataset, pop_size, max_gen, K)
     for i = 1:K
         % Generating initial population
         pop = geraPopulacao(dados, pop_size);
-
-        
+       
+        %%%%%%%%%%%%%%%%
         % Dados do GA original
+        %%%%%%%%%%%%%%%%
         tic;
         [tempFit,ind] = ga(dados, pop_size, max_gen, pop);
         time_original = toc;
+
         aveFitnessOrig = aveFitnessOrig + tempFit;
         aveRuntimeOrig = aveRuntimeOrig + time_original;
         
@@ -55,12 +60,52 @@ function [] = mainSimples(dataset, pop_size, max_gen, K)
         tempRandi = calculo_rand(u,labels);
         
         aveRIOrig = aveRIOrig + tempRandi;
+        
+        [rInd, ~] = size(ind);
+        clustersOrig(i) = rInd; % Número de clusters do resultado
 
+        %%%%%%%%%%%%%%%%
         % Dados do GA modificado
+        %%%%%%%%%%%%%%%%
         tic;
-        ga_contrib(dados, pop_size, max_gen, pop);
+        [tempFit,ind] = ga_contrib(dados, pop_size, max_gen, pop);
         time_contrib = toc;
+        
+        aveFitnessCont = aveFitnessCont + tempFit;
+        aveRuntimeCont = aveRuntimeCont + time_contrib;
+        
+        u = findLabels(ind, dados, labels);
+        tempRandi = calculo_rand(u,labels);
+        
+        aveRICont = aveRICont + tempRandi;
+        
+        [rInd, ~] = size(ind);
+        clustersCont(i) = rInd;
+        
     end
+    
+    aveFitnessOrig = aveFitnessOrig/K;
+    aveFitnessCont = aveFitnessCont/K;
+    
+    aveRuntimeOrig = aveRuntimeOrig/K;
+    aveRuntimeCont = aveRuntimeCont/K;
+    
+    aveRIOrig = aveRIOrig/K;
+    aveRICont = aveRICont/K;
+    
+    %%%%%%%%%%%%%%%%
+    % Montagem do arquivo texto com os dados para tabulação
+    %%%%%%%%%%%%%%%%
+    filename = strcat('dados/', dataset, '.txt');
+
+    fileID = fopen(filename,'w');
+    fprintf(fileID,'Média do Fitness - Média do Tempo - Média do RI - Média # Clusters\n');
+    fprintf(fileID,'Primeira Linha: ORIGINAL\n');
+    fprintf(fileID,'Segunda Linha: CONTRIBUIÇÃO\n');
+
+    fprintf(fileID,'%.2f;%.2f;%.2f;%.2f',aveFitnessOrig,aveRuntimeOrig,aveRIOrig,mean(clustersOrig));
+    fprintf(fileID,'%.2f;%.2f;%.2f;%.2f',aveFitnessCont,aveRuntimeCont,aveRICont,mean(clustersCont));
+    fclose(fileID);    
 
 end
 
