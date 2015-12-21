a65e0e756b08e356a326a1f1e7310e86edcfb7f7
Thiago Moura <tmoura@gmail.com>
2015-12-21 09:52:03 -0300

    Encontra Labels

---
+++ core/findLabels.m
@@ -0,0 +1,40 @@
+%%%%%%%%%%
+%
+% FUNÇÃO: findLabels - Encontra o Labels nos Clusters
+%
+% ENTRADA:
+%
+% - ind: Cromossomo com os clusters
+% - dados: Base de dados sendo utilizada
+% - labels: Labels reais dos dados
+%
+% SAÍDA:
+%
+% - resultLabels: Labels gerados dos clusters
+%
+%%%%%%%%%%
+function [resultLabels] = findLabels(ind, dados, labels)
+
+    [ri,ci] = size(ind);
+    [rd,cd] = size(dados);
+    resultLabels = zeros(rd,1);
+    tempLabels = zeros(rd,1);
+
+    for i = 1:rd
+        temp = [dados(i,:);ind];
+        distances = pdist(temp);
+        distances = distances(1:ri);    % Pega só as distancias do ponto para todos os clusters
+        
+        [~,index] = sort(distances);    % Ordena as distancias procurando a menor
+        
+        tempLabels(i) = index(1);       % Pega o indice do cluster mais próximo
+    end
+    
+    clusterIndex = zeros(ri,1);
+    
+    
+    
+
+
+
+end
